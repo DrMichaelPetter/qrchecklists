d911c6603dc48734610bc9d72c64899e4c9a947c
Michael Petter <petter@cit.tum.de>
2024-10-23 21:28:03 +0200

    funktioniert

--- src/components/PersonItem.jsx
+++ src/components/PersonItem.jsx
@@ -1,7 +1,19 @@
 const PersonItem = ({itemProp, setPeople}) => {
     const person = itemProp;  
+    const handleChange = (key) => {
+        setPeople(prevPeople => {
+            return prevPeople.map(person => {
+                if (person.key === key) {
+                    return {
+                        ...person,
+                        checked: !person.checked
+                    };
+                }
+                return person;
+            });
+        })};
     return (
-        <li><input type="checkbox" checked={itemProp.checked} onChange={()=> console.log("clicked")} /> {person.name} ({person.hof[0]}, K{person.kurs})</li>
+        <li><input type="checkbox" checked={itemProp.checked} onChange={()=> handleChange(person.key)} /> {person.name} ({person.hof[0]}, K{person.kurs})</li>
     );
 }
 export default PersonItem;
