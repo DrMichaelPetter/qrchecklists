3d891acf179c3cf2d5df1ffcd3708fdb8b8cbf9e
Dr. Michael Petter <petter@in.tum.de>
2024-10-23 15:25:02 +0200

    Initial wiring

---
+++ src/components/ChecklistApp.jsx
@@ -0,0 +1,9 @@
+const ChecklistApp = () => {
+    return (
+        <>
+            <h1>Ferienakademie-Checklists</h1>
+            <p>Die Ferienakademie-Checklists sind eine Sammlung von Checklisten, die dir helfen sollen, dich auf die Ferienakademie vorzubereiten.</p>
+        </>
+    );
+}
+export default ChecklistApp;
