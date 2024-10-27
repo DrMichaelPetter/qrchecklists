e9a37aef9226ef047c89eb8dc68a0532adad4645
Michael Petter <petter@cit.tum.de>
2024-10-27 22:07:29 +0100

    even more iconography

--- src/components/PersonItem.jsx
+++ src/components/PersonItem.jsx
@@ -1,13 +1,16 @@
 import {BsPersonDash} from 'react-icons/bs';
 import {BsPersonAdd}  from 'react-icons/bs';
+import { BsHouse } from 'react-icons/bs';
+import { BsMortarboardFill } from 'react-icons/bs';
+import { BsPerson } from 'react-icons/bs';
 
 const PersonItem = ({itemProp, handleChange}) => {
     const person = itemProp;  
     return (
         <li>
             { (!itemProp.checked) ? <button onClick={() => handleChange(person.key)}><BsPersonAdd /></button> : <></>}
-            ({person.hof[0]}, K{person.kurs}) {person.name} 
-         { (itemProp.checked) ? <button onClick={() => handleChange(person.key)}><BsPersonDash /></button> : <></>}
+            <div><BsHouse/>{person.hof[0]}</div> <div><BsMortarboardFill />{person.kurs}</div> <div><BsPerson />{person.name}</div>
+            { (itemProp.checked) ? <button onClick={() => handleChange(person.key)}><BsPersonDash /></button> : <></>}
         </li>
     );
 }
