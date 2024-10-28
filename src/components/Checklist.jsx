9f6aab3a8c0d427130ed7915825e1dda505e8037
Dr. Michael Petter <petter@in.tum.de>
2024-10-28 16:38:10 +0100

    with CSV loading

--- src/components/Checklist.jsx
+++ src/components/Checklist.jsx
@@ -7,11 +7,26 @@ const Checklist = () => {
     useEffect(() => {
         let ignore = false;
         if (!ignore){
-            setPeople(a => [...a,{key:1,name:'Michael Petter',hof:'Murrerhof',kurs:1,checked:false}]);
-            setPeople(a => [...a,{key:2,name:'Tobias Neckel',hof: 'Alpenblick',kurs: 2,checked: false}]);
-            setPeople(a => [...a,{key:3,name:'Julia Pelzer',hof: 'Alpenblick',kurs: 4,checked: false}]);
-            setPeople(a => [...a,{key:4,name:'Thomas Zeiser',hof: 'Rabensteinerhof',kurs: 5,checked: false}]);
-            setPeople(a => [...a,{key:5,name:'Marc Windsheimer',hof: 'Feldrand',kurs: 3,checked: false}]);
+            if (!(people.length === 0)) return;
+            fetch('./teilnehmer.csv').then(response => response.text()).then(text => {
+                console.log('loaded csv '+ text);
+                const rows = text.split('\n');
+                var i=0;
+                rows.slice(1).forEach(row => {
+                    const columns = row.split(',');
+                    if (columns.length === 5) {
+                        i++;
+                        setPeople(a => [...a,{key:parseInt(i),name:columns[0]+" "+columns[1],hof:columns[3],kurs:parseInt(columns[4]),checked:false}]);
+                        console.log(i);
+                    }
+                });
+            });
+
+//            setPeople(a => [...a,{key:1,name:'Michael Petter',hof:'Murrerhof',kurs:1,checked:false}]);
+//            setPeople(a => [...a,{key:2,name:'Tobias Neckel',hof: 'Alpenblick',kurs: 2,checked: false}]);
+//            setPeople(a => [...a,{key:3,name:'Julia Pelzer',hof: 'Alpenblick',kurs: 4,checked: false}]);
+//            setPeople(a => [...a,{key:4,name:'Thomas Zeiser',hof: 'Rabensteinerhof',kurs: 5,checked: false}]);
+//            setPeople(a => [...a,{key:5,name:'Marc Windsheimer',hof: 'Feldrand',kurs: 3,checked: false}]);
         }
         console.log("Initializing people") ;
         return () => { ignore = true; };
