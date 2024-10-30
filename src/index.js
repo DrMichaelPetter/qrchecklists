d976e1c060f845c14071710b972e6f86cb619a33
Michael Petter <petter@cit.tum.de>
2024-10-30 20:33:09 +0100

    offline-shit

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import ChecklistApp from "components/ChecklistApp";
 import "styles/app.css";
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const domContainer = document.getElementById("root");
 const root = ReactDOM.createRoot(domContainer);
 root.render(<ChecklistApp/>);
+
+serviceWorkerRegistration.register();
