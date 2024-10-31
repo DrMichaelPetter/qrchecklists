c106d7e94da9be39e20b82a0bb41c422d3a0bcaf
Dr. Michael Petter <petter@in.tum.de>
2024-10-31 15:17:46 +0100

    nice coursesymbol

--- src/components/PersonItem.jsx
+++ src/components/PersonItem.jsx
@@ -1,7 +1,8 @@
 import styles from 'styles/PersonItem.module.css'
 import { IconContext } from 'react-icons';
 import { BsPlusCircleFill, BsMortarboard, BsFillXCircleFill } from 'react-icons/bs';
-import HofSymbol from './HofSymbol';
+import HofSymbol from 'components/HofSymbol';
+import KursSymbol from 'components/KursSymbol';
 //import { BsXSquareFill, BsPerson } from 'react-icons/bs';
 
 const PersonItem = ({itemProp, handleChange}) => {
@@ -9,9 +10,8 @@ const PersonItem = ({itemProp, handleChange}) => {
     return (
         <IconContext.Provider value={{ style: { verticalAlign: 'middle' } }}>
         <li className={styles.item} style={itemProp.checked ? { background: 'var(--checkmarks-highlightgreen)'}: {}}>
-            
             { (!itemProp.checked) ? <button className={styles.leftbutton} onClick={() => handleChange(person.key)}><BsPlusCircleFill /></button> : <></>}
-            <HofSymbol hofkuerzel={person.hof[0]} /> <div className={styles.metadata}><BsMortarboard />{person.kurs}</div> <div className={styles.metadata}>{person.name}</div>
+            <HofSymbol hofkuerzel={person.hof[0]} /><KursSymbol kurskuerzel={person.kurs} /><div className={styles.metadata}>{person.name}</div>
             { (itemProp.checked) ? <button className={styles.rightbutton} onClick={() => handleChange(person.key)}><BsFillXCircleFill /></button> : <></>}
         </li>
         </IconContext.Provider>
