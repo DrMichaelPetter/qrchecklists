d85269473309ed108f642029520fdc3d7063fda7
Dr. Michael Petter <petter@in.tum.de>
2024-10-31 10:07:36 +0100

    TUM Corporate

--- src/components/PersonItem.jsx
+++ src/components/PersonItem.jsx
@@ -6,12 +6,11 @@ import { BsHouse } from 'react-icons/bs';
 import { BsMortarboard } from 'react-icons/bs';
 import { BsPerson } from 'react-icons/bs';
 
-
 const PersonItem = ({itemProp, handleChange}) => {
     const person = itemProp;  
     return (
         <IconContext.Provider value={{ style: { verticalAlign: 'middle' } }}>
-        <li className={styles.item} style={itemProp.checked ? { background: 'rgb(244,255,244)'}: {}}>
+        <li className={styles.item} style={itemProp.checked ? { background: 'var(--checkmarks-highlightgreen)'}: {}}>
             
             { (!itemProp.checked) ? <button className={styles.leftbutton} onClick={() => handleChange(person.key)}><BsPlusCircleFill /></button> : <></>}
             <div className={styles.metadata}><BsHouse />{person.hof[0]}</div> <div className={styles.metadata}><BsMortarboard />{person.kurs}</div> <div className={styles.metadata}>{person.name}</div>
