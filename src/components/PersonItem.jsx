4d0c0aac13a561153830f38a09503d98aa9ab251
Dr. Michael Petter <petter@in.tum.de>
2025-08-08 13:19:10 +0200

    handle persons without hotel

--- src/components/PersonItem.jsx
+++ src/components/PersonItem.jsx
@@ -12,7 +12,7 @@ const PersonItem = ({itemProp, handleChange, isCurrent}) => {
         <IconContext.Provider value={{ style: { verticalAlign: 'middle' } }}>
         <li className={styles.item} style={isCurrent(person.key) ? { background: person.highlighted?'var(--checkmarks-highlight)':'var(--checkmarks-highlightgreen)'}: {}}>
             { (!isCurrent(person.key)) ? <button className={styles.leftbutton} onClick={() => handleChange(person.key)}><BsPlusCircleFill /></button> : <></>}
-            <HofSymbol hofkuerzel={person.hof[0]} /><KursSymbol kurskuerzel={person.kurs} /><div className={styles.metadata}>{person.name}</div>
+            <HofSymbol hofkuerzel={ `${person.hof}`.substring(0,1)} /><KursSymbol kurskuerzel={person.kurs} /><div className={styles.metadata}>{person.name}</div>
             { (isCurrent(person.key)) ? <button className={styles.rightbutton} onClick={() => handleChange(person.key)}><BsFillXCircleFill /></button> : <></>}
             { (person.highlighted)?<div className={styles.warningsym} ><PiWarningBold color='red'/></div>:null }
         </li>
